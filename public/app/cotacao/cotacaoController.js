4c9b9f17613a0174f9543b38fbff5ab07740d1c9
Lucas Inocente <olucassanchez@gmail.com>
2017-05-07 21:40:24 -0300

    Load itens from Json

--- public/app/cotacao/cotacaoController.js
+++ public/app/cotacao/cotacaoController.js
@@ -1,46 +1,9 @@
-angular.module('cotacaoAgora', []).controller('cotacaoController', function() {
+angular.module('cotacaoAgora', []).controller('cotacaoController', function($scope, $http) {
 
-  console.log('cotacaoController')
+  $http.get('http://demo3643409.mockable.io/quotations').then(function(res){
+    $scope.cotacao.moedas = res.data.result;
+  });
 
-  var cotacao = this;
-    
-  cotacao.moedas = [
-    {
-      "quantity": "1500.00",
-      "total_value": "4932.47",
-      "currencyObj": {
-        "name": "Dólar Americano",
-        "abbreviation": "USD",
-        "acronym": "US$",
-        "website": "http://www.federalreserve.gov",
-        "full_flag_image": "https://s3-sa-east-1.amazonaws.com/beecambioimages/currency-flags/USD.png"
-      }
-    },
-    {
-      "quantity": "1500.00",
-      "total_value": "5372.43",
-      "currencyObj": {
-        "name": "Euro",
-        "abbreviation": "EUR",
-        "acronym": "€",
-        "website": "http://www.ecb.eu",
-        "image_paths": [
-          "https://s3-sa-east-1.amazonaws.com/beecambioimages/currencies/euro/500.png"
-        ],
-        "full_flag_image": "https://s3-sa-east-1.amazonaws.com/beecambioimages/currency-flags/EUR.png"
-      }
-    },
-    {
-      "quantity": "900.00",
-      "total_value": "3710.11",
-      "currencyObj": {
-        "name": "Libra Esterlina",
-        "abbreviation": "GBP",
-        "acronym": "£",
-        "website": "http://www.bankofengland.co.uk",
-        "full_flag_image": "https://s3-sa-east-1.amazonaws.com/beecambioimages/currency-flags/GBP.png"
-      }
-    }
-  ];
+  console.log('$scope.cotacao.moedas', $scope.cotacao.moedas);
 
 });
